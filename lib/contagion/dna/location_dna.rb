2d129357f7db817a7cf40b9b78ee323d3b5d9277
Philip Nguyen <philip.nguyen@procore.com>
2018-07-16 11:36:51 -0700

    add proxy_command
    Some ssh configurations utilize a proxy server, for example a bastion
    server.

--- lib/contagion/dna/location_dna.rb
+++ lib/contagion/dna/location_dna.rb
@@ -1,6 +1,7 @@
 module Contagion
   class LocationDNA < DNA
-    attr_reader :host, :username, :sudo, :file_path, :passphrase
+    attr_reader :host, :username, :sudo, :file_path, :passphrase,
+      :proxy_command
 
     def initialize(raw_dna)
       @passphrase = raw_dna['passphrase']
@@ -8,6 +9,7 @@ module Contagion
       @username = raw_dna['username']
       @sudo = raw_dna['sudo'] == true ? 'sudo' : nil
       @file_path = raw_dna['file_path']
+      @proxy_command = raw_dna['proxy_command']
     end
   end
 end
--- lib/contagion/ssh.rb
+++ lib/contagion/ssh.rb
@@ -1,3 +1,5 @@
+require 'net/ssh/proxy/command'
+
 module Contagion
   class SSH
     attr_reader :content, :location_dna
@@ -40,7 +42,14 @@ module Contagion
     def net_ssh
       @session ||= Net::SSH.start location_dna.host,
                                   location_dna.username,
-                                  passphrase: location_dna.passphrase
+                                  passphrase: location_dna.passphrase,
+                                  proxy: proxy_command
+    end
+
+    def proxy_command
+      if location_dna.proxy_command
+        @_proxy ||= Net::SSH::Proxy::Command.new location_dna.proxy_command
+      end
     end
   end
 end
